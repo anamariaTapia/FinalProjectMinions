3f56c088d19e831c3409554ee450d9ae57ac2dcd
Ana Maria Tapia <anam.tapias18@gmail.com>
2019-06-12 01:59:10 -0400

    maria_tapia/task/initial_structure
    Description:
    - Add AceptanCreateria class
    - Add Owner class
    - Add Task class

---
+++ src/Owner.java
@@ -0,0 +1,21 @@
+public class Owner {
+
+    private String name;
+    private String charge;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharge() {
+        return charge;
+    }
+
+    public void setCharge(String charge) {
+        this.charge = charge;
+    }
+}
